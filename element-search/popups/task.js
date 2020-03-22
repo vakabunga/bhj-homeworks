e15c1930451db20f435fd5e8ae9dc0c9ed030c58
Vakabunga <shesterov@gmail.com>
2020-03-22 22:01:59 +0300

    task done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,1 +1,13 @@
-const modal = document.getElementById
+const modal = document.getElementById('modal_main');
+const done = document.getElementById('modal_success');
+modal.className = modal.className + ' modal_active';
+const doGood = document.getElementsByClassName('btn btn_danger modal__close show-success');
+doGood[0].onclick = () => done.className = done.className + ' modal_active';
+
+const close = document.getElementsByClassName('modal__close_times');
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = () => {
+        done.className = 'modal';
+        modal.className = 'modal';
+    }
+}
