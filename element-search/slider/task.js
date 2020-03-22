aafaa923b1a96e94c67a52742232a7bd27dfd5a8
Vakabunga <shesterov@gmail.com>
2020-03-23 02:27:20 +0300

    task done

---
+++ element-search/slider/task.js
@@ -0,0 +1,43 @@
+const arrows = Array.from(document.querySelectorAll('.slider__arrow'));
+const slider = Array.from(document.querySelectorAll('.slider__item'));
+let currentSlide;
+const dots = Array.from(document.querySelectorAll('.slider__dot'));
+
+for (let i = 0; i < slider.length; i++) { //вычисляем текущий слайд
+    if (slider[i].className.includes('slider__item_active')) {
+        currentSlide = i;
+        dots[i].className += ' slider__dot_active';
+    }
+}
+
+arrows[0].onclick = showSlide;
+arrows[1].onclick = showSlide;
+
+function usualSlide() { //возвращаем точке и слайду неактивное состояние
+    slider[currentSlide].className = 'slider__item';
+    dots[currentSlide].className = 'slider__dot';
+}
+
+function activeSlide() { //делаем слайд и точку активной
+    slider[currentSlide].className += ' slider__item_active';
+    dots[currentSlide].className += ' slider__dot_active';
+}
+
+function showSlide() { //обрабатываем событие на стрелках
+    usualSlide();
+    if (this.className.includes('slider__arrow_prev')) {
+        currentSlide === 0 ? currentSlide = slider.length - 1 : currentSlide -= 1;
+    }
+    else currentSlide === slider.length - 1 ? currentSlide = 0 : currentSlide += 1;
+    activeSlide();
+}
+
+for (let i = 0; i < dots.length; i++) { //заряжаем событие на каждую точку
+    dots[i].onclick = showDot;
+}
+
+function showDot() {
+    usualSlide();
+    currentSlide = dots.indexOf(this);
+    activeSlide();
+}
