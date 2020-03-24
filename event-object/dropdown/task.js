f76d97c433fa3d0ea52cb67223fb573b1e7e221c
Vakabunga <shesterov@gmail.com>
2020-03-24 21:50:11 +0300

    fixed some syntax

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,7 +3,9 @@ const hiddenMenu = Array.from(document.getElementsByClassName('dropdown__list'))
 button[0].addEventListener('click', showMenu);
 
 function showMenu() {
-    !hiddenMenu[0].className.includes('dropdown__list_active') ? hiddenMenu[0].className += ' dropdown__list_active' : hiddenMenu[0].className = 'dropdown__list';
+    !hiddenMenu[0].className.includes('dropdown__list_active') ?
+     hiddenMenu[0].className += ' dropdown__list_active' : 
+     hiddenMenu[0].className = 'dropdown__list';
 }
 
 hiddenMenu[0].addEventListener('click', selectMenuItem);
