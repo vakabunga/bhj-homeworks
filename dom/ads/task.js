1df84d7e4b9a93f54918fcc1d81c682bc3bee4cd
Vakabunga <shesterov@gmail.com>
2020-03-30 13:10:53 +0300

    add rotation timer dependence from dataset.speed via function sleep()

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,5 +1,10 @@
 const rotatorCases = Array.from(document.querySelectorAll('.rotator__case'));
 let index = 0;
+function sleep(milliseconds) {
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+}
+let timer = 1000;
 const rotation = function() {
   rotatorCases[index].classList.remove('rotator__case_active');
   index++;
@@ -8,6 +13,8 @@ const rotation = function() {
     'style',
     `color: ${rotatorCases[index].dataset.color}`
   );
+  timer = Number(rotatorCases[index].dataset.speed);
+  sleep((timer));
   rotatorCases[index].classList.add('rotator__case_active');
 };
-setInterval(rotation, 1000);
+setInterval(rotation, 0);
