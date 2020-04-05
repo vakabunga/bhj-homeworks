a13d5fa0d011292327585ce5505f977c1facee5b
Vakabunga <shesterov@gmail.com>
2020-04-06 00:15:17 +0300

    task done

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,12 @@
+const editor = document.getElementById('editor'); // фиксируем в переменной поле ввода
+editor.value = localStorage.getItem('textarea'); // подгружаем в поле ввода текст, который был сохранен, если был сохранен
+editor.insertAdjacentHTML('afterend', '<div><button type="button" id="reset">Очистить поле</button></div>'); // добавляем кнопку на страницу
+const button = document.getElementById('reset'); // фикисруем кнопку в переменную
+// при клике на кнопку, очищаем поле, но не localStorage
+button.addEventListener('click', (e) => {
+  editor.value = '';
+});
+// записываем в localStorage текст в textarea при снятии фокуса с текстового поля
+editor.addEventListener('change', () => {
+  localStorage.setItem('textarea', editor.value);
+});
