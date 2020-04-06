540fdf8f5cfdccc90fa5eed6a09882a7bb1a8c5a
Vakabunga <shesterov@gmail.com>
2020-04-06 23:53:28 +0300

    add setting cookies, stop show modal if it was shown once

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,18 +1,17 @@
 const modal = document.getElementById('subscribe-modal');
 const closeModal = document.querySelector('.modal__close_times');
+const name = 'modal';
 const getCookie = (name) => {
-  debugger;
   const value = '; ' + document.cookie;
   let parts = value.split('; ' + name + '=');
   if (parts.length === 2) {
     return parts.pop().split(';').shift();
   }
 };
-// if () {
-modal.classList.add('modal_active');
-
-// }
+if (getCookie(name) != 'isShown') {
+  modal.classList.add('modal_active');
+}
 closeModal.onclick = () => {
   modal.classList.remove('modal_active');
-  document.cookie = 'name=modalIsShown';
+  document.cookie = 'modal=isShown';
 };
